d1615730ff137da004e115bcf41667792774191b
RedxYeti <39033204+RedxYeti@users.noreply.github.com>
2025-07-06 22:04:11 -0400

    Add files via upload

---
+++ AutomaticReloadFix/__init__.py
@@ -0,0 +1,17 @@
+
+from mods_base import build_mod, hook
+from unrealsdk.hooks import Type
+from unrealsdk.unreal import UObject
+
+@hook("Engine.PlayerController:StopFire", Type.POST)
+@hook("Engine.PlayerController:StartFire", Type.POST)
+def test2(obj:UObject, *_) -> None:
+    weapon = obj.Pawn.Weapon
+    offhand = obj.Pawn.OffHandWeapon
+    if weapon and weapon.NeedToReload():
+        weapon.BeginReload(0)
+    if offhand and offhand.NeedToReload():
+        offhand.BeginReload(0)
+    return
+
+build_mod()
